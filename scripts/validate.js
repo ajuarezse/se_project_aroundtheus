41b1b89c60efbaa647b67efbd424a545968dda86
angel juarez <ajuarez.se@gmail.com>
2023-10-25 22:05:55 -0700

    fix modal BEM

--- scripts/validate.js
+++ scripts/validate.js
@@ -71,7 +71,7 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save",
-  inactiveButtonClass: "modal__save_disabled",
+  inactiveButtonClass: "modal__button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__span_opened",
 };
